dd6074fd07fd1bea71c41d91027980ed29186050
Maxine Chui <maxine.chui@gmail.com>
2018-02-03 18:36:15 -0800

    not in place

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -4,10 +4,22 @@ class QuickSort
 
   # Not in-place. Uses O(n) memory.
   def self.sort1(array)
+    return array if array.length <= 1
+    pivot = array[0]
+    left = []
+    right = []
+    array.each do |el|
+      left << el if el <= pivot
+      right << el if el > pivot
+    end
+    puts left
+    puts right
+    self.sort1(left) + pivot + self.sort1(right)
   end
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    
   end
 
   def self.partition(array, start, length, &prc)
